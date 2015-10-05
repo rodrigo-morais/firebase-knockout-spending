fb1599022712c23ebb1130aa15f06d1342b6150b
Rodrigo Morais <morais.rm@gmail.com>
2015-10-05 18:24:01 -0300

    Adjusting the list of spending per day.

--- app/days/daysVM.js
+++ app/days/daysVM.js
@@ -2,44 +2,40 @@ define(['knockout', 'firebase', 'moment', '../config', '../day/dayVM'], function
     return function daysViewModel() {
         var self = this,
         	firebase_url = new config().url,
-        	today = moment().format('YYYY-MM-DD');
+        	today = moment().format('YYYY-MM-DD'),
+        	spending = new Firebase(firebase_url);
 
         self.days = ko.observableArray([]);
-        self.today_total = ko.observable();
-        
-    	var spending = new Firebase(firebase_url);
+        self.today_total = ko.observable(0);
+
+        var _addToday = function(spent){
+        	var total = 0,
+        		_day = new day();
+
+        	if(spent.date === today){
+		    	_day.date(spent.date);
+		    	_day.item(spent.item);
+		    	_day.value('$ ' +  parseFloat(spent.value).toFixed(2));
+
+		    	total = self.today_total() + parseFloat(spent.value);
+
+		    	self.days.push(_day);
+	    	}
+
+	    	self.today_total(total.toFixed(2));
+        }
 
     	spending.orderByChild("date").equalTo(today).on("child_added", function(snapshot){
 		    var spent = snapshot.val();
 
-		    console.log(spent);
-		    console.log(spent.value);
-		    
+		    _addToday(spent);		    
 		},
 		function(errorObject){
 		    console.log("The read failed: " + errorObject.code);
 		});
 
 		spending.orderByChild("date").on("value", function(snapshot){
-		    var spending = snapshot.val().spending,
-	    		total = 0;
-
-		    for(var key in spending){
-		    	var spent = spending[key],
-		    		_day = new day();
-
-		    	if(spent.date === today){
-			    	_day.date(spent.date);
-			    	_day.item(spent.item);
-			    	_day.value('$ ' +  parseFloat(spent.value).toFixed(2));
-
-			    	total = total + parseFloat(spent.value);
-
-			    	self.days.push(_day);
-		    	}
-
-		    	self.today_total(total.toFixed(2));
-		    }
+		    var spending = snapshot.val();
 		},
 		(errorObject) => {
 		    console.log("The read failed: " + errorObject.code);
